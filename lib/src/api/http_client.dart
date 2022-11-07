3e38210b45cb1aa037333353151a5e03b939d389
hanslelee <hssarah13@gmail.com>
2022-11-07 22:18:40 +0900

    import 수정

--- lib/src/api/http_client.dart
+++ lib/src/api/http_client.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class HttpClient {
   static final HttpClient _instance = HttpClient._internal();
-  final String baseUrl = 'http://a77e-35-204-31-203.ngrok.io';
+  final String baseUrl = 'http://9b6a-35-204-31-203.ngrok.io';
   static const String rtmpUrl = 'rtmp://43.201.22.53';
 
   String accessToken = '';
